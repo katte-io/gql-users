c4cabd9faa9831c124175ad2cbc2bf767b281869
shashankkatte <shashankanand.katte@gmail.com>
2021-03-22 07:12:59 -0400

    src  : Adding Schema for GraphQL

---
+++ schema/schema.js
@@ -0,0 +1,12 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GrapgQLInt } = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstname: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
