c697991cb0a9ebb2ff46159e46a168115455182f
shashankkatte <shashankanand.katte@gmail.com>
2021-03-22 11:23:07 -0400

    src : Adding company type and nested queries

--- schema/schema.js
+++ schema/schema.js
@@ -9,12 +9,29 @@ const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 //   { id: '47', firstname: 'Diana', age: 21 },
 // ];
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((resp) => resp.data);
+      },
+    },
   },
 });
 
